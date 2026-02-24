7a50525033520c3062570da30a7cb12a8d640aec
ST10512621 <ST10512621@myemeris.edu.za>
2026-02-24 12:35:04 +0200

    First submission

---
+++ src/stopandgo/StopAndGo.java
@@ -0,0 +1,30 @@
+
+//A first attempt at using escape sequences 
+
+package stopandgo;
+
+/**
+ *
+ * @version StopAndGo_1.0
+ */
+public class StopAndGo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+     
+       System.out.println("""
+                              ######
+                            #         \b#
+                          #    STOP     \b# 
+                            #        \b #
+                               \b###### 
+                                 \b## 
+                                \b#### 
+                                 \b## 
+                              \b######## """);
+    }
+    
+}
